c20645334e04601f207c2c6a9a553f6c2daef98b
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-11-26 15:02:08 -0800

    init npm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import db from '@astrojs/db';
-
 // https://astro.build/config
-export default defineConfig({
-  integrations: [db()]
-});
+export default defineConfig({});
