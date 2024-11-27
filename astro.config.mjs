51a29e3bbc2b61b2e4109d21fc61bba41c6f8897
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-11-27 14:45:43 -0800

    added in clerk authentication

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,10 @@
 import { defineConfig } from "astro/config";
 
 import db from "@astrojs/db";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import node from "@astrojs/node";
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,8 +14,9 @@ export default defineConfig({
         tailwind({
             applyBaseStyles: false,
         }),
-        ,
+        clerk(),
         react(),
     ],
+    adapter: node({ mode: "standalone" }),
     output: "server",
 });
