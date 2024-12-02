98d6f8b24684d1232ef533d9c09d43befeaac5a3
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-12-02 14:36:40 -0800

    removing paymententry.astro

--- src/components/paymententry.tsx
+++ src/components/paymententry.tsx
@@ -9,12 +9,10 @@ import {
     DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
 
-interface PaymentEntryProps {
-    children: React.ReactNode;
-}
-
-export const PaymentEntry = ({ children }: PaymentEntryProps) => {
+export const PaymentEntry = () => {
     return (
         <Drawer>
             <DrawerTrigger>Open</DrawerTrigger>
@@ -25,9 +23,29 @@ export const PaymentEntry = ({ children }: PaymentEntryProps) => {
                         This action cannot be undone.
                     </DrawerDescription>
                 </DrawerHeader>
-                {children}
+                <div>
+                    <Label htmlFor="date">Date</Label>
+                    <Input id="date" name="date" type="date" />
+
+                    <Label htmlFor="amount">Amount</Label>
+                    <Input id="amount" name="amount" type="number" />
+
+                    <Label htmlFor="card">Card Used</Label>
+                    <Input id="card" name="card" />
+
+                    <Label htmlFor="purchaseType">Purchase Type</Label>
+                    <Input id="purchaseType" name="purchaseType" />
+
+                    <Label htmlFor="desc">Description</Label>
+                    <Input id="desc" name="desc" />
+
+                    <Label htmlFor="notes">Notes</Label>
+                    <Input id="notes" name="notes" />
+                </div>
                 <DrawerFooter>
-                    <Button>Submit</Button>
+                    <form method="POST" className="grid p-6">
+                        <Button type="submit">Submit</Button>
+                    </form>
                     <DrawerClose asChild>
                         <Button variant="outline">Cancel</Button>
                     </DrawerClose>
