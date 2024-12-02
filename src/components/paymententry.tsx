f3cad6ad8a8559bc1407026e007553b5a44377e7
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-12-02 14:49:28 -0800

    inputs will change the useState

--- src/components/paymententry.tsx
+++ src/components/paymententry.tsx
@@ -11,8 +11,19 @@ import {
 import { Button } from "./ui/button";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
+import type { Payment } from "@/types/db";
+import { useState } from "react";
 
 export const PaymentEntry = () => {
+    const [payment, setPayment] = useState({
+        date: new Date(),
+        amount: 0,
+        cardId: 0,
+        purchaseTypeId: 0,
+        description: "",
+        notes: "",
+    } as Payment);
+
     return (
         <Drawer>
             <DrawerTrigger>Open</DrawerTrigger>
@@ -25,22 +36,81 @@ export const PaymentEntry = () => {
                 </DrawerHeader>
                 <div>
                     <Label htmlFor="date">Date</Label>
-                    <Input id="date" name="date" type="date" />
+                    <Input
+                        id="date"
+                        name="date"
+                        type="date"
+                        value={payment.date.toISOString().split("T")[0]}
+                        onChange={(e) =>
+                            setPayment({
+                                ...payment,
+                                date: new Date(e.target.value),
+                            })
+                        }
+                    />
 
                     <Label htmlFor="amount">Amount</Label>
-                    <Input id="amount" name="amount" type="number" />
+                    <Input
+                        id="amount"
+                        name="amount"
+                        type="number"
+                        value={payment.amount}
+                        onChange={(e) =>
+                            setPayment({
+                                ...payment,
+                                amount: parseFloat(e.target.value),
+                            })
+                        }
+                    />
 
                     <Label htmlFor="card">Card Used</Label>
-                    <Input id="card" name="card" />
+                    <Input
+                        id="card"
+                        name="card"
+                        value={payment.cardId}
+                        onChange={(e) =>
+                            setPayment({
+                                ...payment,
+                                cardId: parseInt(e.target.value),
+                            })
+                        }
+                    />
 
                     <Label htmlFor="purchaseType">Purchase Type</Label>
-                    <Input id="purchaseType" name="purchaseType" />
+                    <Input
+                        id="purchaseType"
+                        name="purchaseType"
+                        value={payment.purchaseTypeId}
+                        onChange={(e) =>
+                            setPayment({
+                                ...payment,
+                                purchaseTypeId: parseInt(e.target.value),
+                            })
+                        }
+                    />
 
                     <Label htmlFor="desc">Description</Label>
-                    <Input id="desc" name="desc" />
+                    <Input
+                        id="desc"
+                        name="desc"
+                        value={payment.description}
+                        onChange={(e) =>
+                            setPayment({
+                                ...payment,
+                                description: e.target.value,
+                            })
+                        }
+                    />
 
                     <Label htmlFor="notes">Notes</Label>
-                    <Input id="notes" name="notes" />
+                    <Input
+                        id="notes"
+                        name="notes"
+                        value={payment.notes}
+                        onChange={(e) =>
+                            setPayment({ ...payment, notes: e.target.value })
+                        }
+                    />
                 </div>
                 <DrawerFooter>
                     <form method="POST" className="grid p-6">
