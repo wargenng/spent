a4d81dc9851dce7ec2341281794b8472e696ddaf
wargenng <wargenng@gmail.com>
2024-12-04 13:26:55 -0800

    adding solid js

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,17 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import clerk from "@clerk/astro";
-
 import vercel from "@astrojs/vercel/serverless";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        db(),
-        tailwind({
-            applyBaseStyles: false,
-        }),
-        clerk(),
-        react(),
-    ],
+    integrations: [db(), tailwind({
+        applyBaseStyles: false,
+    }), clerk(), react(), solidJs()],
     adapter: vercel(),
     output: "server",
 });
