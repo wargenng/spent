fb630ee39cd97d28f12a41abf6cb425fb06047d9
wargenng <wargenng@gmail.com>
2024-12-10 13:40:59 -0800

    changing toast to dialog

--- src/components/paymententry/paymententryhandler.tsx
+++ src/components/paymententry/paymententryhandler.tsx
@@ -1,14 +1,16 @@
 import type { Card, Category } from "@/types/db";
-import { toaster } from "@kobalte/core";
+import PaymentEntryForm from "./paymententry";
+
 import {
-    Toast,
-    ToastContent,
-    ToastDescription,
-    ToastList,
-    ToastProgress,
-    ToastRegion,
-    ToastTitle,
-} from "@/components/ui/toast";
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { createSignal } from "solid-js";
 import { Button } from "../ui/button";
 
 interface PaymentEntryProps {
@@ -24,26 +26,42 @@ export default function PaymentEntry({
     cards,
     children,
 }: PaymentEntryProps) {
-    const showToast = () => {
-        toaster.show((props) => (
-            <Toast toastId={props.toastId}>
-                <ToastContent>
-                    <ToastTitle>Scheduled: Catch up</ToastTitle>
-                    <ToastDescription>
-                        Friday, February 10, 2023 at 5:57 PM
-                    </ToastDescription>
-                </ToastContent>
-                <ToastProgress />
-            </Toast>
-        ));
-    };
+    const [open, setOpen] = createSignal(false);
 
     return (
-        <>
-            <div onClick={showToast}>{children}</div>
-            <ToastRegion swipeDirection="up">
-                <ToastList />
-            </ToastRegion>
-        </>
+        <div>
+            {cards.length === 0 ? (
+                <Dialog open={open()} onOpenChange={setOpen}>
+                    <DialogTrigger> {children}</DialogTrigger>
+                    <DialogContent class="w-5/6">
+                        <DialogHeader>
+                            <DialogTitle>
+                                Payment Entry Requires Cards
+                            </DialogTitle>
+                            <DialogDescription>
+                                Please add a card before entering a payment.
+                            </DialogDescription>
+                        </DialogHeader>
+                        <DialogFooter class="flex gap-2">
+                            <Button
+                                onclick={() => {
+                                    setOpen(false);
+                                }}
+                            >
+                                Close
+                            </Button>
+                        </DialogFooter>
+                    </DialogContent>
+                </Dialog>
+            ) : (
+                <PaymentEntryForm
+                    userId={userId}
+                    categories={categories}
+                    cards={cards}
+                >
+                    {children}
+                </PaymentEntryForm>
+            )}
+        </div>
     );
 }
