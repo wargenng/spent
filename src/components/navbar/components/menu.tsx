c55455d81b3c7b251445ccaca3c16bec743aafb7
wargenng <wargenng@gmail.com>
2024-12-16 17:07:06 -0800

    changed add button

--- src/components/navbar/components/menu.tsx
+++ src/components/navbar/components/menu.tsx
@@ -28,16 +28,16 @@ export default function Menu({
                 <DrawerTrigger class="md:hidden md:w-full block w-auto">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 text-gray-500 dark:text-gray-400"
                         viewBox="0 0 24 24"
+                        class="h-7 w-7 text-gray-500 dark:text-gray-400"
                     >
-                        <g fill="currentColor">
-                            <path d="M7 14a2 2 0 1 0 0-4a2 2 0 0 0 0 4m7-2a2 2 0 1 1-4 0a2 2 0 0 1 4 0m3 2a2 2 0 1 0 0-4a2 2 0 0 0 0 4" />
-                            <path
-                                fill-rule="evenodd"
-                                d="M24 12c0 6.627-5.373 12-12 12S0 18.627 0 12S5.373 0 12 0s12 5.373 12 12m-2 0c0 5.523-4.477 10-10 10S2 17.523 2 12S6.477 2 12 2s10 4.477 10 10"
-                                clip-rule="evenodd"
-                            />
+                        <g
+                            fill="currentColor"
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                        >
+                            <path d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12m10-8a8 8 0 1 0 0 16a8 8 0 0 0 0-16" />
+                            <path d="M13 7a1 1 0 1 0-2 0v4H7a1 1 0 1 0 0 2h4v4a1 1 0 1 0 2 0v-4h4a1 1 0 1 0 0-2h-4z" />
                         </g>
                     </svg>
                 </DrawerTrigger>
