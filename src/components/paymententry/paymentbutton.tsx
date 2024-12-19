caed22f9f0f9f62918ad0d11fd5b1cf78d4ac390
wargenng <wargenng@gmail.com>
2024-12-18 16:38:16 -0800

    changing transaction image

--- src/components/paymententry/paymentbutton.tsx
+++ src/components/paymententry/paymentbutton.tsx
@@ -55,7 +55,7 @@ export default function PaymentButton({
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="2"
-                                        d="M7 3v18m3-15L7 3L4 6m16 12l-3 3l-3-3m3 3V3"
+                                        d="M11 16h10m-10 0l4 4m-4-4l4-4m-2-4H3m10 0l-4 4m4-4L9 4"
                                     />
                                 </svg>
                             </div>
@@ -81,7 +81,7 @@ export default function PaymentButton({
                                         stroke-linecap="round"
                                         stroke-linejoin="round"
                                         stroke-width="2"
-                                        d="M7 3v18m3-15L7 3L4 6m16 12l-3 3l-3-3m3 3V3"
+                                        d="M11 16h10m-10 0l4 4m-4-4l4-4m-2-4H3m10 0l-4 4m4-4L9 4"
                                     />
                                 </svg>
                             </div>
@@ -105,7 +105,7 @@ export default function PaymentButton({
                                     stroke-linecap="round"
                                     stroke-linejoin="round"
                                     stroke-width="2"
-                                    d="M7 3v18m3-15L7 3L4 6m16 12l-3 3l-3-3m3 3V3"
+                                    d="M11 16h10m-10 0l4 4m-4-4l4-4m-2-4H3m10 0l-4 4m4-4L9 4"
                                 />
                             </svg>
                         </div>
