8f55d78f5b5f50b49bfd0827047ef8f47b86f191
Wargen Guittap <48695406+wargenng@users.noreply.github.com>
2024-12-18 17:41:19 -0800

    updating to astro 15

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,24 @@ import { defineConfig } from "astro/config";
 import db from "@astrojs/db";
 import tailwind from "@astrojs/tailwind";
 import clerk from "@clerk/astro";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [db(), tailwind({
-        applyBaseStyles: false,
-    }), clerk(), solidJs()],
-    adapter: vercel(),
+    integrations: [
+        db(),
+        tailwind({
+            applyBaseStyles: false,
+        }),
+        clerk(),
+        solidJs(),
+    ],
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+        maxDuration: 8,
+    }),
     output: "server",
 });
